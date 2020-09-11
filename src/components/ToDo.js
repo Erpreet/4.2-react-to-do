d8d9dadde7db14c2fcf8a9873ed57765f149faaf
Erpreet <preetkaurchd15@gmail.com>
2020-09-11 11:42:46 -0600

    New State Data

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState } from 'react';
 
 //component function
 function ToDo ()
 {
 
+    const [newTask, setNewTask] = useState('default');
+
     //We use "return" for our render, in a component.
     return (
         <>
+            <h2>{newTask}</h2>
+
             <form>
                 <label htmlFor="task">New Task:</label>
-                <input type="text" id="task" />
+                <input 
+                type="text"
+                 id="task" 
+                 onChange={e => {setNewTask(e.target.value)}}
+                 value = {newTask} />
+                 <p>
+                     <strong>Current Task Value:</strong>
+                     <em>{newTask}</em>
+                 </p>
                 <input type="submit" value="Add To-Do" />
             </form>
             <ul></ul>
