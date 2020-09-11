bbe68c012b09bf032a34e1fe118cac9e97975bb1
Erpreet <preetkaurchd15@gmail.com>
2020-09-11 11:08:45 -0600

    Add To-Do Form and List component

---
+++ src/components/ToDo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+//component function
+function ToDo ()
+{
+
+    //We use "return" for our render, in a component.
+    return (
+        <>
+            <form>
+                <label htmlFor="task">New Task:</label>
+                <input type="text" id="task" />
+                <input type="submit" value="Add To-Do" />
+            </form>
+            <ul></ul>
+        </>
+    );
+
+}
+
+export default ToDo;
